59c60d0c2fa0ca63464ba63ea5621cf607d2101e
rickym-h <richardhull11@gmail.com>
2022-12-02 20:22:28 +0000

    added main game loop code and created a loop to play the 'rounds' of the game. will break out of the loop if the user decides to, or if the game is complete

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,45 @@
 import java.util.ArrayList;
 
+enum Location {
+    FISHING_VILLAGE,
+    TRADING_VILLAGE,
+    DOCKS
+}
+
 public class Game {
+    private Location location;
+    private int money;
+    private ArrayList<Fish> fishInventory;
+    private boolean escaped = false;
+
     Game() {
         // Constructor for new game instance
+        location = Location.FISHING_VILLAGE;
+        money = 0;
+        fishInventory = new ArrayList<Fish>();
+
+        // todo give the user some detail about the world as they start the game
     }
 
-    Game(int money, ArrayList<Fish> fishInventory) {
+    Game(int money, ArrayList<Fish> fishInventory, Location location) {
         // Constructor for game with saved game data
+        // todo set the data
+        // todo remind the player about where they are in the story
+    }
+
+    public boolean playGameLoop() {
+        // Main loop of a 'round'
+        // Gives the user options of what they can do, and will update the game state accordingly
+        // Returns true normally
+        // Returns false if the player wants to save and quit.
+        System.out.println("PLAYING GAME LOOP");
+        System.out.println("Forcing escape...");
+        this.escaped = true;
+        return true;
+
+    }
+
+    public boolean hasWon() {
+        return this.escaped;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Started Fishing Game!");
+        System.out.println("Welcome to my Fishing Game!");
 
-        // Query if the user wants to start a new game, or load from a save.
+        // todo Query if the user wants to start a new game, or load from a save.
 
         // Start game with either a fresh player with no money or fish, or load game with a set number of fish and money
+        Game gameInstance = new Game();
+
+        while (!gameInstance.hasWon()) {
+            // If the player does not want to keep playing, process saving the user data and then quit.
+            if (!gameInstance.playGameLoop()) {
+                // todo save the user data and quit
+                System.out.println("Goodbye!");
+                return;
+            }
+        }
+
+        // Congratulate user for completing the game
+        System.out.println("Congratulations on escaping the plagued village!");
     }
 }
